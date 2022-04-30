dfaf39c25fd29380d506d01df340496235c7ca9c
Diego Olivera <diegooliverautn@gmail.com>
2022-04-29 23:06:01 -0300

    [ADD]titulos viaje datos en home

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 
 //components
 import Header from './componentes/header/Header';
+import Home from './componentes/home/Home';
 
 function App() {
   return (
     <div className="App">
       <Header/>      
+      <Home/>
       
     </div>
   );
---
+++ src/componentes/home/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+//css
+import "./home.css"
+const Home = () => {
+  return (
+    <main>
+        <div className='flex'>
+            <p>Viaje</p>
+            <p>Datos</p>
+        </div>
+
+    </main>
+  )
+}
+
+export default Home
